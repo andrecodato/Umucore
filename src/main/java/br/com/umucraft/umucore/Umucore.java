f7f839d350b92357e3d66b3e909286eae2bb6da6
andrecodato <andrecodato@gmail.com>
2026-06-29 01:40:01 -0300

    First Commit

---
+++ src/main/java/br/com/umucraft/umucore/Umucore.java
@@ -0,0 +1,16 @@
+package br.com.umucraft.umucore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Umucore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
